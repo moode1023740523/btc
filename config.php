695af5044c9128c7e6b7d59a0f6c47eba7166ab6
moode1023740523 <54100463+moode1023740523@users.noreply.github.com>
2019-11-11 08:16:48 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9; SM-G965F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.62 Mobile Safari/537.36";
+$PHPSESSID = "S3M2UHdzOXZWbGJ2bUdCaEpNbjdrV0FRL1VYelhJYlRmL2dKdWlVMkNXV1hHVGg0TXRqVldvMlMyNzl5ZnFzRQ%3D%3D";
